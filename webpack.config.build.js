a96b3c132372d3dfb14b0ffdbe9fccae011ee673
James Younge <jamesskyoung@outlook.com>
2017-06-07 20:44:33 +0100

    wip

---
+++ webpack.config.build.js
@@ -0,0 +1,44 @@
+var path = require('path');
+
+var config = {
+    entry: './lib/SuperTable.jsx',
+
+
+    output: {
+        path: path.join(__dirname, "./dist/js"),
+        filename: 'reactsupertable.js',
+
+    },
+
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
+        port: 9080
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                include: /node_modules/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+}
+console.log('output path...' + config.output.path);
+module.exports = config;
